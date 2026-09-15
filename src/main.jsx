13d5ed22857ceea2a1aa766b04dce6bda02fc286
Minhyeok Kim <basakgrape@gmail.com>
2026-09-15 14:57:45 +0900

    style: split playtest styles

--- src/main.jsx
+++ src/main.jsx
@@ -1,7 +1,9 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
-import './styles.css'
+import './base.css'
+import './game.css'
+import './cards.css'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
