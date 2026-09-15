7ba575f4e390a080df89b9e8718831c7f6df73f7
Minhyeok Kim <basakgrape@gmail.com>
2026-09-15 15:35:59 +0900

    ux: expose online 2P entry from main screen

--- src/main.jsx
+++ src/main.jsx
@@ -7,10 +7,12 @@ import './cards.css'
 import './game.css'
 
 const params = new URLSearchParams(window.location.search)
-const RootApp = params.get('online') === '1' || params.has('room') ? OnlineApp : App
+const onlineMode = params.get('online') === '1' || params.has('room')
+const RootApp = onlineMode ? OnlineApp : App
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
+    {!onlineMode && <a className="quick-online-entry" href="?online=1">ONLINE 2P</a>}
     <RootApp />
   </React.StrictMode>,
 )
