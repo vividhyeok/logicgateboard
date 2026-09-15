bab914937d3ee3a6315ed33ef19f6919605939f2
Minhyeok Kim <basakgrape@gmail.com>
2026-09-15 15:35:21 +0900

    feat: route query-string online mode and fix style imports

--- src/main.jsx
+++ src/main.jsx
@@ -1,12 +1,16 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
+import OnlineApp from './OnlineApp.jsx'
 import './base.css'
-import './game.css'
 import './cards.css'
+import './game.css'
+
+const params = new URLSearchParams(window.location.search)
+const RootApp = params.get('online') === '1' || params.has('room') ? OnlineApp : App
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
+    <RootApp />
   </React.StrictMode>,
 )
